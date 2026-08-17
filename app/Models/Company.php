24780912e1167303ad7507f0aeda3396602bd815
lecareli <leticia.careli@outlook.com>
2026-08-17 14:39:58 -0300

    Remove unused HasFactory annotation from Company model for cleaner code

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    /** @use HasFactory<CompanyFactory> */
     use HasFactory;
 
     protected $table = 'companies'; // filiais
